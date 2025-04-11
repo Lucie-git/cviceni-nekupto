99ddffcce6f2c6d9b1b358748d081e8b63717e3f
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-04-11 11:13:51 +0200

    assignment 2 done

--- index.js
+++ index.js
@@ -18,8 +18,32 @@ const product3 = {
   description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
 
-const product1Card = document.querySelector('#product1').classList.add('border-primary');
-const product2Button = document.querySelector('#product2 .btn').classList.remove('btn-primary');
-const product3Title = document
-  .querySelector('#product3 .card-title')
-  .classList.toggle('text-center');
+// const product1Card = document.querySelector('#product1').classList.add('border-primary');
+// const product2Button = document.querySelector('#product2 .btn').classList.remove('btn-primary');
+// const product3Title = document
+//   .querySelector('#product3 .card-title')
+//   .classList.toggle('text-center');
+
+const product1Elm = document.querySelector('#product1');
+product1Elm.innerHTML += `<img class="card-img-top" src=${product1.image} alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product1.name}</h2>
+    <p class="card-text">${product1.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>`;
+
+const product2Elm = document.querySelector('#product2');
+product2Elm.innerHTML += `<img class="card-img-top" src=${product2.image} alt="Card image cap">
+    <div class="card-body">
+      <h2 class="card-title">${product2.name}</h2>
+      <p class="card-text">${product2.description}</p>
+    </div>
+    <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>`;
+
+const product3Elm = document.querySelector('#product3');
+product3Elm.innerHTML += `<img class="card-img-top" src=${product3.image} alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product3.name}</h2>
+    <p class="card-text">${product3.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>`;
